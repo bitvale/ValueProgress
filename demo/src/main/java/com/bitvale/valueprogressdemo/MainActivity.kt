cd598c1b3392cf847b51de3d62b884c0fb4c120f
Alexander Kolpakov <jquickapp@gmail.com>
2018-11-24 19:56:23 +0200

    Add splash screen

--- demo/src/main/java/com/bitvale/valueprogressdemo/MainActivity.kt
+++ demo/src/main/java/com/bitvale/valueprogressdemo/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
